b294eeae0249809f69b5836da64259a2bdeb4dc4
Junjie.Zhao <570738660@qq.com>
2021-06-24 15:18:54 +0800

    pager frg supported

--- zj_cf/src/main/java/com/zj/cf/fragments/BaseTabFragment.kt
+++ zj_cf/src/main/java/com/zj/cf/fragments/BaseTabFragment.kt
@@ -1,11 +1,9 @@
 package com.zj.cf.fragments
 
 abstract class BaseTabFragment : BaseFragment() {
-
-
-    fun updateState(){
-
-    }
-
-
+    /**
+     * @exception IllegalArgumentException the fid It is not recommended to be overwritten. If it is overwritten,
+     * it must be ensured that the FID produced by each instance is unique.
+     */
+    override val fId: String; get() = super.fId
 }
